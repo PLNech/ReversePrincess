e0176181d1a742cd057a568d43f6efba04658c90
Paul-Louis NECH <pln@algolia.com>
2024-04-20 17:28:26 +0200

    feat: Debug local init mode

--- TODO.md
+++ TODO.md
@@ -5,6 +5,9 @@
 - RAG: Try leveraging RAG for game metadata/values tracking
 - LIFE: Integrate life/death conditions
 
+### Gradio
+- Use consecutive messages : https://www.gradio.app/guides/blocks-and-event-listeners#running-events-consecutively
+- 
 
 ## Achievements
 - DRAGONLOVE
--- game.py
+++ game.py
@@ -6,11 +6,14 @@ import gradio as gr
 import ollama
 
 PROMPT_INTRO = ("The year is 2077 and the world is a cyberpunk utopia. "
-                "But you are a princess, and you're imprisoned in a dragon's dungeon castle! "
+                "But you are a princess, and you're imprisoned in a cyberdragon's dungeon cyber castle! "
                 "And worse, your beloved prince got trapped - he doesn't seem so good at saving anyone, even his ass. "
                 "Can you escape the castle to go rescue this cute loser?"
                 "\n You start your journey in the following room:")
 SYSTEM = "You are a story generator."
+BUTTON_BEGIN = "Once upon a time, the princess was locked in"
+
+DEBUG_LOCAL_INIT = False
 
 
 def vote(data: gr.LikeData):
@@ -22,112 +25,152 @@ def vote(data: gr.LikeData):
 
 
 def oracle(question: str, is_json: bool = True) -> str:
-    response = ollama.chat(model="gemma", format="json" if is_json else "", messages=[
+    model_name = "dolphin-mistral:7b-v2-q3_K_S"
+    model_name = "llama3"
+    response = ollama.chat(model=model_name, format="json" if is_json else "", messages=[
         {
             "role": "user",
             "content": question,
         },
     ])
-    response: str = response["message"]
+    response: [dict, str] = response["message"]
     # print(f"{question} -> {response}")
-    content = response['content'] if "content" in response else response
-    return content
+    content = response["content"] if "content" in response else response
+    clean = content.strip("\n ")
+    return clean
 
 
 def make_options(input_prompt: str = None,
-                 retries: int = 3) -> list[str]:
+                 retries: int = 3) -> [list[str], str]:
     # options = ["ACTION 1", "ACTION 2", "ACTION 3"]
     prompt = (f"{SYSTEM} {input_prompt}. Generate three options and reply in JSON, "
-              f"with your three options under the key 'options', as an array of strings.")
+              f"with your three options under the key 'options', as an array of strings. Don't use the word 'option'.")
     for _ in range(retries):
-        values: dict[str, Any] = json.loads(oracle(prompt))
+        oracle_response = oracle(prompt)
+        values: dict[str, Any] = json.loads(oracle_response)
         if 'options' in values:  # "MODEL IS STUPID"
-            return values['options']
+            print(f"{prompt}\n->\n{oracle_response}")  # DEBUG
+            return values['options'], oracle_response
     raise SystemError(f"Failed to make options after {retries} retries...")
 
 
 def game_step(choice: str, inventory: list[str],
-              retries: int = 3) -> tuple[str, list[str]]:
-    # return random.choice(["How are you?", "I love you", "I'm very hungry"])
+              retries: int = 5) -> tuple[str, list[str]]:
     for _ in range(retries):
+        example_response = {'story':
+                                'You unlock the door and see a room full of books.',
+                            'inventory': ['A weirdly-shaped golden key', 'Your map of the underground networks',
+                                          'A mirror connected to the world\'s magic information field']}
         prompt = (f"The princess chose {choice}. Her inventory was {inventory}. "
                   f"What happens next to her? Don't mention the choice, just the action and consequences. "
                   f"Reply in a short, descriptive sentence, as a string under the key 'story'. "
-                  "Add a key 'inventory' with a list of strings describing her updated inventory.")
-        next_step = oracle(prompt)
+                  "If she gets or loses an item as a consequence, add an 'inventory' key "
+                  "with an updated list of strings describing her belongings she currently holds. "
+                  f"Example response: {json.dumps(example_response)}")
+        next_step: str = oracle(prompt)
         print(next_step)
         try:
-            next_step = json.loads(next_step)
+            next_data: dict[str, Any] = json.loads(next_step)
             if all([key in next_step for key in ["story", "inventory"]]):
-                story: str = next_step["story"]
-                new_inventory: list[str] = next_step["inventory"]
+                story: str = next_data["story"]
+                new_inventory: list[str] = next_data["inventory"]
+                if len(new_inventory) < 3:
+                    inventory = [*inventory, *new_inventory][:3]
                 return story, new_inventory
             else:
-                print(f"Missing story/inventory: {next_step}")
+                print(f"Missing story/inventory: {next_data}")
         except (ValueError, TypeError, JSONDecodeError) as e:
             print(f"Error {type(e)}: {e}")
             continue
     raise SystemError(f"Failed to generate story+inventory after {retries} tries...")
 
 
-def respond(button: str, chat_history, inventory: list[str]):
-    bot_message, new_inventory = game_step(button, inventory)
-    print(f"C  Choice:{button}")
+def respond(choice: str, chat_history, inventory: list[str]):
+    bot_message, new_inventory = game_step(choice, inventory)
+    print(f"C  Choice:{choice}")
     print(f"S  Story:{bot_message}")
     print(f"NI New Inventory:{new_inventory}")
     if new_inventory is None or new_inventory[0] == "None":
         new_inventory = inventory
     try:
-        prompt_specific_options = f"The story so far: {chat_history}. The inventory was: {inventory} and is now: {new_inventory}." \
-                                  f"What can I do next, phrased from my first-person perspective? Give me three options: " \
-                                  f"the first one should be safe, even if potentially dull. " \
+        prompt_specific_options = f"The story so far: {chat_history}. My choice is now: {choice}. " \
+                                  "The inventory was: {inventory} and is now: {new_inventory}. " \
+                                  f"What can I do next, phrased from my first-person perspective? " \
+                                  f"Give me three options: the first one should be safe, even if potentially dull. " \
                                   f"the second one should be daring and more adventurous. " \
                                   f"the third one should be funky and wacky, try to be fun if you can make a joke. " \
-                                  "Try to balance options to use different inventory items, when some are relevant. "
-        options = make_options(prompt_specific_options)
+                                  "Try to balance options to use different inventory items, when some are relevant. " \
+                                  "Keep each option short, as it will be displayed on a button. "
+        options, json_str = make_options(prompt_specific_options)
         print(f"Buttons: {options}")
         assert len(options) == 3, "Bad options!"
     except [AssertionError, JSONDecodeError] as e:
         print(f"Bad options! {options}")
         raise
-    chat_history.append((button, bot_message))
+    chat_history.append((choice, bot_message))
 
-    return options[0], options[1], options[2], chat_history, "\n".join(["- " + n for n in new_inventory])
+    try:
+        assert type(inventory) is list and type(inventory[0]) is str
+    except AssertionError as exc:
+        print(f"Failed inventory: {exc} - defaulting to {inventory}")
+        new_inventory = ["A butterfly"]
+    return options[0], options[1], options[2], chat_history, "\n".join(["- " + n for n in new_inventory]), json_str
 
 
 if __name__ == '__main__':
     import gradio as gr
 
     print("Running game!")
-    with gr.Blocks(title="Reverse Princess Simulator") as demo:
-        with gr.Row() as row1:
-            chatbot = gr.Chatbot(label="Damsell in Prowess", value=[(None, PROMPT_INTRO)], scale=3)
-            with gr.Column(scale=1) as col:
-                print("Loading initial inventory... ", end="")
-                stuff: list[str] = make_options(
-                    "The princess has three objects with her: the first is a versatile tool, "
-                    "the second an item of tremendous personal value, "
-                    "the third a surprising thing to have for a princess which makes the story fun.")
-                print(f"Stuff: {stuff}")
+
+
+    print("Loading initial inventory... ", end="")
+    if DEBUG_LOCAL_INIT:
+        print("DEBUG LocalInit activated! Initial options are hardcoded for quick iteration")
+        options = ['A trusty multitool that can cut, saw, and hammer its way out of any situation.',
+                   "The family heirloom locket with  a lock of her mother's hair, passed down through generations.",
+                   'A pet tarantula named Mr. Whiskers who has a knack for sensing danger and warns of impending doom.']
+    else:
+        options, _ = make_options(
+            "The princess has three objects with her: the first is a versatile tool, "
+            "the second an item of tremendous personal value, "
+            "the third a surprising thing to have for a princess which makes the story fun.")
+    stuff: list[str] = options
+    print(f"Stuff: {stuff}")
+
+    print("Loading initial choice... ", end="")
+    if DEBUG_LOCAL_INIT:
+        options = ["The damp cell with rusty bars", "The luxurious suite with a malfunctioning holographic display",
+                   "The cold storage room filled with ancient technology"]
+        json_str = json.dumps({"options": options})
+    else:
+        options, json_str = make_options(
+            PROMPT_INTRO + " The story starts with just three options of rooms in the castle "
+                           "where the princess could be locked in.")
+    print(f"Buttons: {options}")
+
+    # UI
+    with gr.Blocks(title="Reverse Princess Simulator", css="footer{display:none !important}") as demo:
+        with gr.Row() as body:  # Outer layout is horizontal
+            with gr.Column(scale=4) as col1:  # Story & Choices
+                chatbot = gr.Chatbot(label="Damsell in Prowess", value=[(None, PROMPT_INTRO)], scale=3)
+                with gr.Row() as buttons:  # Options
+                    action1 = gr.Button(f"{BUTTON_BEGIN} {options[0]}.")
+                    action2 = gr.Button(f"{BUTTON_BEGIN} {options[1]}.")
+                    action3 = gr.Button(f"{BUTTON_BEGIN} {options[2]}.")
+
+            with gr.Column(scale=1) as col2:  # Infos & Metadata
                 # FIXME: Replace with List? Dataframe? Any tabular data UI
                 inventory = gr.TextArea(label="Inventory",
                                         value="\n".join(["- " + s for s in stuff]),
                                         scale=1)
+                json_view = gr.Json(value=json_str, label="Last oracle reply", scale=2)
 
-        with gr.Row() as row2:
-            print("Loading initial choice... ", end="")
-            options = make_options(PROMPT_INTRO + " The story starts with just three options of rooms in the castle "
-                                                  "where the princess could be locked in.")
-            print(f"Buttons: {options}")
+        # Interactions must be mapped inside a gradio.Blocks context
+        outputs = [action1, action2, action3, chatbot, inventory, json_view]
+        action1.click(respond, [action1, chatbot, inventory], outputs)
+        action2.click(respond, [action2, chatbot, inventory], outputs)
+        action3.click(respond, [action3, chatbot, inventory], outputs)
 
-            action1 = gr.Button(f"Once upon a time, the princess was locked in {options[0]}. ")
-            action2 = gr.Button(f"Once upon a time, the princess was locked in {options[1]}. ")
-            action3 = gr.Button(f"Once upon a time, the princess was locked in {options[2]}. ")
-
-        action1.click(respond, [action1, chatbot, inventory], [action1, action2, action3, chatbot, inventory])
-        action2.click(respond, [action2, chatbot, inventory], [action1, action2, action3, chatbot, inventory])
-        action3.click(respond, [action3, chatbot, inventory], [action1, action2, action3, chatbot, inventory])
-
-        # chatbot.like(vote, None, None)
+    # chatbot.like(vote, None, None)
     demo.queue()
     demo.launch()
