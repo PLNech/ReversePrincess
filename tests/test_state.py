aa0bdf2e46fab7ded6c45fc8965f0c7b24483635
Paul-Louis NECH <pln@algolia.com>
2024-05-07 16:26:50 +0200

    tests: GameState

---
+++ tests/test_state.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from prompts import INTRO
+from state import GameState
+
+
+class TestIntro(TestCase):
+    def test_intro(self):
+        game_state = GameState(INTRO)
+        history = game_state.history_so_far()
+        self.assertGreater(len(history), 100)
+
+    def test_update(self):
+        game_state = GameState(INTRO)
+        game_state.update(["She is now in outer space floating above the Earth"],
+                          "Low-Earth Orbit", "Wants to land on the Moon!")
+        history = game_state.history_so_far()
+
+        self.assertIn("outer space", history)
+        self.assertNotIn("Moon", history)
+        self.assertNotIn("Low-Earth Orbit", history)
+        print(history)
