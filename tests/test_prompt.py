f4803df14a6979544de039824b16df043debbf79
tagadanar <raphael.renon@gmail.com>
2024-05-07 16:25:19 +0200

    initial test

---
+++ tests/test_prompt.py
@@ -0,0 +1,21 @@
+import unittest
+from textstat import textstat
+from narrator import GameNarrator
+from state import GameState
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        prompt = f""
+        state = GameState(f"")
+        location = GameNarrator.current_location(state)
+
+        readability = textstat.flesch_reading_ease(response)
+        self.assertGreater(readability, 50)
+
+        words = textstat.difficult_words_list(response, 3)
+        self.assertLess(len(words), 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
