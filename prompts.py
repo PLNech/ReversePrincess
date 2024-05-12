6e19513f5cb04cf65cccd9e089f9c7dea9e2ed99
Paul-Louis NECH <pln@algolia.com>
2024-05-12 17:16:52 +0200

    feat(images): Better prompts?

--- prompts.py
+++ prompts.py
@@ -1,8 +1,12 @@
+"""Example:
+Stuck in AI box chat with guardian, comicbook, technological comic, art by Picasso,
+in the style of , Marvel DC comics, accent lighting,
+"""
 IMAGE_STYLES = {
     "TIMELESS": "timeless child book illustration, bright watercolor, aged book, cute illustration",
     "PIXELS": "low-resolution pixel art, dark background lofi rendering",
-    "ENKI": "in the style of enki bilal, futuristic watercolor pencils",
-    "MOEBIUS": "in the style of moebius comic book futuristic pencil drawing, bold coloring, realistic drawing"
+    "ENKI": "in the style of enki bilal, Enki Bilal's style, futuristic tones, watercolor pencils, best quality",
+    "MOEBIUS": "comicbook, art by Moebius, cyberpunk comic, bold coloring, accent lighting, elegant complex picture"
 }
 IMAGE_STYLE_NAMES: list[str] = [s for s in sorted(IMAGE_STYLES.keys())]
 IMAGE_STYLE_DEFAULT: str = "MOEBIUS"
